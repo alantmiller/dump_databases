aa25378b9129a5b2e0d8216b0a448599158f1121
Alan Miller <183872+alantmiller@users.noreply.github.com>
2023-07-22 02:05:43 -0700

    Update database_backups.py

--- src/database_backups.py
+++ src/database_backups.py
@@ -32,3 +32,57 @@ class DatabaseDump:
             logging.info("Database dumped successfully")
         except Exception as e:
             logging.error(f"An error occurred while dumping the database: {str(e)}")
+
+    def compress_db_dump(self):
+        """Compress the dumped database"""
+        try:
+            with open(self.db_dump_path, 'rb') as f_in:
+                with gzip.open(self.db_dump_path + '.gz', 'wb') as f_out:
+                    shutil.copyfileobj(f_in, f_out)
+            logging.info("Database dump compressed successfully")
+        except Exception as e:
+            logging.error(f"An error occurred while compressing the database dump: {str(e)}")
+
+    def email_db_dump(self):
+        """Email the compressed database dump"""
+        try:
+            msg = MIMEMultipart()
+            msg['From'] = 'my_email@example.com'
+            msg['To'] = self.email
+            msg['Subject'] = 'Database Dump'
+
+            text = MIMEText('Please find attached the latest database dump.')
+            msg.attach(text)
+
+            # Connect to the mail server
+            server = smtplib.SMTP('my_smtp_server.com', 587)
+            server.starttls()
+
+            # Login Credentials for sending the mail
+            password = "my_email_password"
+            server.login(msg['From'], password)
+
+            # Send the message
+            text = msg.as_string()
+            server.sendmail(msg['From'], msg['To'], text)
+            server.quit()
+
+            logging.info("Database dump emailed successfully")
+        except Exception as e:
+            logging.error(f"An error occurred while emailing the database dump: {str(e)}")
+
+    def delete_db_dump(self):
+        """Delete the local database dump"""
+        try:
+            os.remove(self.db_dump_path)
+            logging.info("Database dump deleted successfully")
+        except Exception as e:
+            logging.error(f"An error occurred while deleting the database dump: {str(e)}")
+
+
+if __name__ == '__main__':
+    db_dump = DatabaseDump('/path/to/your/db_dump.sql', 'your_email@example.com')
+    db_dump.dump_db()
+    db_dump.compress_db_dump()
+    db_dump.email_db_dump()
+    db_dump.delete_db_dump()
