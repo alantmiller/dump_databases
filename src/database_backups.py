4bc2ca5291aca3603159493e575dc8ad94c1e036
Alan Miller <183872+alantmiller@users.noreply.github.com>
2023-07-22 01:46:37 -0700

    Update database_backups.py

--- src/database_backups.py
+++ src/database_backups.py
@@ -1,50 +1,27 @@
-
 import os
-import subprocess
-import datetime
-import tarfile
-import smtplib
 import json
-import unittest
-import logging
-from email.message import EmailMessage
-from concurrent.futures import ThreadPoolExecutor
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import subprocess
+import shlex
 
-class Database:
-    def __init__(self, database, username, password):
-        self.database = database.strip()
-        self.username = username.strip()
-        self.password = password.strip()
+class DatabaseDump:
 
-
-class DumpDatabases:
-    def __init__(self):
-        self.databases = []
-        self.messages = []
-        self.config = self.load_config()
-
-        self.mysqldump_cmd = self.config['mysqldump_cmd']
-        self.dump_dir = self.config['dump_dir']
-        self.owner = self.config['owner']
-        self.group = self.config['group']
-        self.email_from = self.config['email_from']
-        self.email_admin = self.config['email_admin']
-        self.host = self.config['host']
-
-        self.messages.append('STARTED @ {}'.format(datetime.datetime.now().strftime("%B %d, %Y, %I:%M%p")))
-
-        # Additional configuration such as dump options and tar options can be added here.
-
-    def load_config(self):
-        # Load JSON configuration file
-        with open('config.json') as json_file:
-            data = json.load(json_file)
-        return data
-
-    def add_database(self, db):
-        self.databases.append(db)
-        self.messages.append('ADDED DATABASE: {}'.format(db.database))
-
-    # The rest of the methods will follow the similar structure.
-
-# The rest of the python code will be written here including error handling, testing and logging, etc.
+    def __init__(self, config_file):
+        """Initialize the class with configuration settings"""
+        # Read the configuration file
+        with open(config_file, "r") as file:
+            self.config = json.load(file)
+        
+        # Setup variables based on configuration
+        self.db_host = self.config["database"]["host"]
+        self.db_user = self.config["database"]["user"]
+        self.db_password = self.config["database"]["password"]
+        self.db_name = self.config["database"]["name"]
+        self.db_dump_path = self.config["paths"]["db_dump_path"]
+        self.log_path = self.config["paths"]["log_path"]
+        self.email_admin = self.config["email"]["admin"]
+        self.email_subject = self.config["email"]["subject"]
+        self.email_from = self.config["email"]["from"]
+        self.msgs = []
